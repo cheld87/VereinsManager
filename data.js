0718732db2053b300ceeb36d6b862b84a9245d50
cheld87 <c.held.1987@gmail.com>
2026-07-02 03:21:13 +0200

    data.js

---
+++ data.js
@@ -0,0 +1,43 @@
+// ===================================
+// VereinsManager
+// Stammdaten
+// ===================================
+
+const app = {
+    version: "0.1.0",
+    build: "0001",
+    verein: "FV Matzenberg",
+    startKasse: 150
+};
+
+const artikel = [
+
+    { emoji:"🍺", name:"UrPils", preis:2.80 },
+    { emoji:"📦", name:"Bierkasten", preis:35.00 },
+
+    { emoji:"🍺", name:"Weizenbier", preis:2.80 },
+    { emoji:"🍺", name:"Pils alkoholfrei", preis:2.80 },
+    { emoji:"🍺", name:"Radler", preis:2.80 },
+
+    { emoji:"🍺", name:"Mixery Cola", preis:2.80 },
+    { emoji:"🍺", name:"Mixery Blue", preis:2.80 },
+    { emoji:"🍺", name:"Mixery Cola Orange", preis:2.80 },
+
+    { emoji:"🥤", name:"Coca-Cola", preis:2.30 },
+    { emoji:"🥤", name:"Fanta", preis:2.30 },
+    { emoji:"🥤", name:"Sprite", preis:2.30 },
+
+    { emoji:"💧", name:"Sprudel", preis:1.80 },
+    { emoji:"🍎", name:"Apfelschorle", preis:1.80 },
+    { emoji:"🍏", name:"Gründels Fresh", preis:2.30 },
+
+    { emoji:"🥤", name:"Isogetränk", preis:1.80 },
+    { emoji:"🧃", name:"Eistee Pfirsich", preis:1.80 },
+    { emoji:"🧃", name:"Eistee Zitrone", preis:1.80 },
+
+    { emoji:"⚡", name:"Energy", preis:1.80 },
+    { emoji:"🧃", name:"Capri-Sonne", preis:1.00 },
+
+    { emoji:"☕", name:"Kaffee", preis:1.50 }
+
+];
