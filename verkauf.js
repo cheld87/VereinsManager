70209ff40c2e9f88d20057235b9ec2d066409b4b
cheld87 <c.held.1987@gmail.com>
2026-07-02 03:42:43 +0200

    verkauf.js

---
+++ verkauf.js
@@ -0,0 +1,49 @@
+// ===================================
+// VereinsManager
+// storage.js
+// Speichern & Laden
+// ===================================
+
+function datenSpeichern() {
+
+    const daten = {
+        umsatz: umsatz,
+        warenkorb: warenkorb,
+        artikel: artikel
+    };
+
+    localStorage.setItem(
+        "vereinsmanager",
+        JSON.stringify(daten)
+    );
+
+}
+
+function datenLaden() {
+
+    const daten = JSON.parse(
+        localStorage.getItem("vereinsmanager")
+    );
+
+    if (!daten) {
+        return;
+    }
+
+    umsatz = daten.umsatz || 0;
+    warenkorb = daten.warenkorb || [];
+
+    if (daten.artikel) {
+
+        daten.artikel.forEach((gespeichert, index) => {
+
+            if (artikel[index]) {
+
+                artikel[index].preis = gespeichert.preis;
+
+            }
+
+        });
+
+    }
+
+}
