25db021fd3ba7c84332468a9cff2e5d37ba8403a
cheld87 <c.held.1987@gmail.com>
2026-07-04 01:55:10 +0200

    ui.js

--- js/ui.js
+++ js/ui.js
@@ -3,3 +3,19 @@
 // ui.js
 // Version 0.4.0 Alpha
 // ===================================
+
+function refreshUI(){
+
+    renderGetraenke();
+    renderWarenkorb();
+    updateDashboard();
+
+    if(document.getElementById("settings").style.display === "block"){
+        renderSettings();
+    }
+
+    if(document.getElementById("lagerDialog").style.display === "flex"){
+        renderLagerStatus();
+    }
+
+}
