57b591a2ae9863298c89a3d6a572235553a71585
cheld87 <c.held.1987@gmail.com>
2026-07-04 01:45:35 +0200

    lager.js

--- js/lager.js
+++ js/lager.js
@@ -3,3 +3,60 @@
 // lager.js
 // Version 0.4.0 Alpha
 // ===================================
+
+function lagerPlus(id, feld){
+
+    const a = artikelNachId(id);
+
+    if(!a || !a.lager) return;
+
+    if(feld === "kaesten"){
+
+        a.lager.kaesten++;
+        a.lager.flaschen += a.lager.flaschenProKasten;
+
+    }else{
+
+        a.lager.flaschen++;
+
+    }
+
+    datenSpeichern();
+
+    refreshUI();
+
+}
+
+function lagerMinus(id, feld){
+
+    const a = artikelNachId(id);
+
+    if(!a || !a.lager) return;
+
+    if(feld === "kaesten"){
+
+        if(a.lager.kaesten > 0){
+
+            a.lager.kaesten--;
+
+            a.lager.flaschen = Math.max(
+                0,
+                a.lager.flaschen - a.lager.flaschenProKasten
+            );
+
+        }
+
+    }else{
+
+        if(a.lager.flaschen > 0){
+
+            a.lager.flaschen--;
+
+        }
+
+    }
+
+    datenSpeichern();
+
+    refreshUI();
+}
