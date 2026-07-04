51bdf439c549904357e39cfa8cd0c61b79c489a9
cheld87 <c.held.1987@gmail.com>
2026-07-04 02:05:15 +0200

    statistik.js

--- js/statistik.js
+++ js/statistik.js
@@ -3,3 +3,21 @@
 // statistik.js
 // Version 0.4.0 Alpha
 // ===================================
+
+let statistik = {};
+
+function verkaufRegistrieren(id){
+
+    if(!statistik[id]){
+
+        statistik[id] = {
+
+            verkauft: 0
+
+        };
+
+    }
+
+    statistik[id].verkauft++;
+
+}
