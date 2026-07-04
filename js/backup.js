3966ed04df9e4aaad289c03cdd01d9c77136d2d0
cheld87 <c.held.1987@gmail.com>
2026-07-04 04:14:13 +0200

    backup.js

--- js/backup.js
+++ js/backup.js
@@ -4,6 +4,49 @@
 // Version 0.4.0 Alpha
 // Datensicherung
 // ===================================
-backupErstellen()
+function backupErstellen(){
 
-backupLaden()
+    const daten = {
+
+        version: app.version,
+
+        datum: new Date().toLocaleString(),
+
+        umsatz,
+
+        warenkorb,
+
+        artikel,
+
+        statistik,
+
+        abschluesse
+
+    };
+
+
+    const datei = new Blob(
+        [
+            JSON.stringify(daten,null,2)
+        ],
+        {
+            type:"application/json"
+        }
+    );
+
+
+    const link =
+        document.createElement("a");
+
+
+    link.href =
+        URL.createObjectURL(datei);
+
+
+    link.download =
+        "vereinsmanager_backup.json";
+
+
+    link.click();
+
+}
