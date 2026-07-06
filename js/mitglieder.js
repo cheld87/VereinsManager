fd716d80490272a80bad4b1b766220fe0df45b26
cheld87 <c.held.1987@gmail.com>
2026-07-06 20:37:58 +0200

    mitglieder.js

--- js/mitglieder.js
+++ js/mitglieder.js
@@ -4,3 +4,57 @@
 // Version 0.6.0 Alpha
 // Mitgliederkonten
 // ===================================
+function showMitglieder(){
+
+    let html = `
+    
+    <h2>👥 Mitgliederkonten</h2>
+
+    <button
+    class="neuButton"
+    onclick="mitgliedNeu()">
+
+    ➕ Mitglied hinzufügen
+
+    </button>
+    
+    `;
+
+
+    if(mitglieder.length === 0){
+
+        html += `
+        <p>Noch keine Mitglieder angelegt.</p>
+        `;
+
+    }else{
+
+        mitglieder.forEach(m=>{
+
+            html += `
+
+            <div class="lagerCard">
+
+                <h3>${m.name}</h3>
+
+                <p>
+                Kontostand:
+                <b>${m.konto.toFixed(2)} €</b>
+                </p>
+
+            </div>
+
+            `;
+
+        });
+
+    }
+
+
+    document.getElementById("lagerDialogInhalt").innerHTML =
+        html;
+
+    document.getElementById("lagerDialog").style.display =
+        "flex";
+
+}
